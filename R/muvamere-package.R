1ca7dc5d22f9fb95e1e41ff496286b9400af884c
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2025-01-02 10:14:58 +0000

    outline

---
+++ R/muvamere-package.R
@@ -0,0 +1,18 @@
+#' The 'muvamere' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name muvamere-package
+#' @aliases muvamere
+#' @useDynLib muvamere, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.6. https://mc-stan.org
+#'
+NULL
